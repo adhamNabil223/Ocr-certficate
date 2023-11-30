7a9128b0a041e2f72ad7ef4c42ce57ebcc69e02b
adham nabil <139056583+adhamNabil223@users.noreply.github.com>
2023-11-30 17:41:30 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,128 @@
-import Image from 'next/image'
-import withAuth from '@/components/auth/Authentication'
+import React from 'react';
 import { useState } from 'react';
-import UserTable from '@/components/common/UsersTable';
 import { useTranslation } from 'react-i18next';
+import { instance } from '../api/config';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
+function UserTable({ users }) {
+  const [certficats, setCerticats] = useState(null)
 
+  async function getCertficatis() {
+    const user = JSON.parse(localStorage.getItem('user'))
+    const data = await instance.get(`documents/${user.id}`)
+    setCerticats(data)
+  }
+  async function getCustomData(url) {
+    let data = await axios.get(url, {
+      headers:{
+      Authorization : `Bearer ${Cookies.get('token')}`
+      }
+    })
+    setCerticats(data)
+  }
+  useEffect(() => {
+    getCertficatis()
+  }, [])
+  
 
- function Home() {
-  const users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'User' },
-  ];
+  return (certficats != null) && (
+    <>
+    {
+      certficats === "not found" ? <div className='text-center'> No Certficates Found</div> :
+      <> 
+       <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+            Name
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+          Uploaded By
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+          Orginal Certficate
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+          Orginal ID
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
+           Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        {certficats.data.results.map((certficate) => (
+          <tr key={certficate.id}>
+            {
+              certficate.data !== null &&
+              <>
+             <td className="px-6 py-4 whitespace-nowrap">{certficate.data.id_data.first_name} {certficate.data.id_data.last_names}</td>
+             <td className="px-6 py-4 whitespace-nowrap">{certficate.uploaded_by.first_name}</td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href={certficate.certificate} target='_blank'>
+                  GO
+              </a>
+            </td>
+            <td className="px-6 py-4 whitespace-nowrap">
+              <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href={certficate.national_id} target='_blank'>
+              GO
+              </a>
+            </td>
 
-  const {t} = useTranslation()
-
-  return (
-   <>
-   <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">{t('table')}</h1>
-      <UserTable users={users} />
+            <td className="px-6 py-4 whitespace-nowrap">
+              <Link href={`/certficate/${certficate.id}`} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Check Result
+              </Link>
+            </td>
+              </>
+            }
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <div className='text-center mt-10'>
+    <button
+    disabled={certficats.previous !== null} 
+    className={`text-white ${certficats.previous !== null ? 'bg-gray-400' : 'bg-blue-700 hover:bg-blue-800'} focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`}
+    onClick={()=>{
+      getCustomData(certficats.previous)
+     }}
+    >
+      Pervious
+    </button>
+    <button
+     disabled={certficats.next !== null} 
+     className={`text-white ${certficats.next !== null ? 'bg-gray-400' : 'bg-blue-700 hover:bg-blue-800'} focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`} 
+     onClick={()=>{
+      getCustomData(certficats.next)
+     }}
+     >
+      Next
+    </button>
     </div>
-   </>
-  )
+      </>
+    }
+   
+    </>
+  );
 }
 
-
-
-export default withAuth(Home)
-
+export default UserTable;
